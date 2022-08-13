ff6d35dd05a5388f3a1db2d24ba61f7eca50c389
fred <fredsterdickinson@gmail.com>
2022-08-13 15:58:39 +0100

    authorise

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ class fredster {
         this.sqlite = require("sqlite3");
         this.fs = require("fs");
         this.request = require("request");
+        this.express = require("express");
+        this.app = this.express();
+        this.querystring = require("querystring");
+        this.spotify = new SpotifyWebApi({
+            redirectUri: process.env.REDIRECT_URI,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+        });
 
         this.utils = new Map();
         this.timers = new Map();
@@ -58,10 +66,66 @@ class fredster {
         })
     }
 
+    async authorise() {
+        return new Promise((resolve) => {
+            this.app.get("/", (req, res) => {
+                // https://developer.spotify.com/documentation/general/guides/authorization/scopes/
+                let scope = "playlist-modify-public playlist-modify-private";
+                let redirect_link = "https://accounts.spotify.com/authorize?" +
+                    this.querystring.stringify({
+                        response_type: 'code',
+                        client_id: process.env.CLIENT_ID,
+                        scope: scope,
+                        redirect_uri: process.env.REDIRECT_URI
+                    });
+
+                console.log(process.env.REDIRECT_URI);
+                res.redirect(redirect_link);             
+            });
+
+            this.app.get("/callback", (req, res) => {
+                res.send("time2steal all ur data lol");
+                
+                let accessToken, refreshToken;
+                let code = req.query.code || null;
+                if (!code) return console.error("Auth code not retrieved.");
+                // from https://github.com/thelinmichael/spotify-web-api-node#usage
+                this.spotify.authorizationCodeGrant(code).then(
+                    function(data) {
+                        console.log('The token expires in ' + data.body['expires_in']);
+                        console.log('The access token is ' + data.body['access_token']);
+                        console.log('The refresh token is ' + data.body['refresh_token']);
+                    
+                        accessToken = data.body['access_token'];
+                        refreshToken = data.body['refresh_token'];
+                    },
+                    function(err) {
+                      console.log('Something went wrong!', err);
+                    }
+                  );        
+
+                  this.spotify.setAccessToken(accessToken);
+                  this.spotify.setRefreshToken(refreshToken);    
+                  
+                  // CALL TIMER TO REFRESH TOKEN EVERY 3500 SECONDS
+            });
+
+            this.app.listen(8888, () => {
+                console.log("Listening on 8888.");
+            });
+
+            resolve();
+        });
+    }
+
     async start() {
+        //this.setupURI();
+        this.authorise();
         await this.loadUtils();
         await this.loadTimers();
         await this.loadDatabase();
+
+        //this.setupURI();
 
         console.log("Startup complete");
     }
